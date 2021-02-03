d780f6cd47913117c16ca282e3b01b36ccea768e
Flufflul <mufflul@gmail.com>
2021-02-02 20:35:24 -0500

    Created "Apple" physics
    + Changed project collision settings
    +> Apple // AppleTree, Basket
    + "Apple" : physics & remove-at-Y

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -16,7 +16,14 @@ public class AppleTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Drop apple/s
+        // Drop apple/second
+        Invoke("DropApple",2f);
+    }
+
+    void DropApple() {
+        GameObject apple = Instantiate<GameObject>(applePrefab);
+        apple.transform.position = transform.position;
+        Invoke("DropApple", secondsBetweenAppleDrops);
     }
 
     // Update is called once per frame
