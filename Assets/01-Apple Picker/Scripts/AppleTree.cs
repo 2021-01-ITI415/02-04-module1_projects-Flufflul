890c6dc7694a9d675ffed0ecab574f4f0307d27f
Flufflul <mufflul@gmail.com>
2021-02-02 19:54:44 -0500

    Created "AppleTree" movement
    + "AppleTree" : .cs
    + "AppleTree" : movement, randomizer

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in inspector")]
+
+    public GameObject applePrefab;
+
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Drop apple/s
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Basic movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Change direction
+        if (pos.x < -leftAndRightEdge) { speed = Mathf.Abs(speed); }
+        else if (pos.x > leftAndRightEdge) { speed = -Mathf.Abs(speed); }
+        // else if (Random.value < chanceToChangeDirections) { speed *= -1; }
+    }
+
+    void FixedUpdate() {
+        // Time-based instead of frame-based
+        if (Random.value < chanceToChangeDirections) { speed *= -1; }
+    }
+}
